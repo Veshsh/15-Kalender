744f5dd4af920ed313e5c1078f16d8b24a7312a1
Veshsh <isip_i.i.evskin@mpt.ru>
2023-04-11 10:45:02 +0300

    Konvert

--- 12 Kalender/obj/Debug/12 Kalender_Content.g.i.cs
+++ 12 Kalender/obj/Debug/12 Kalender_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("5.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("7.png")]
 
 
